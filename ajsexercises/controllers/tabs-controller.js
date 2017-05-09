bf483870ef1bd9b267410726c1dca291ec9f7fed
Mickyssin <micky_moco_@hotmail.com>
2017-05-09 08:39:23 -0600

    Add files via upload

--- ajsexercises/controllers/tabs-controller.js
+++ ajsexercises/controllers/tabs-controller.js
@@ -4,10 +4,20 @@
 
     angular.module('BookStoreApp').controller('TabController',TabController);
 
-    TabController.$inject= ['book'];
-    function TabController(book) {
+    TabController.$inject= ['book','BookService','$state'];
+    function TabController(book,BookService,$state) {
         var $ctrl= this;
         $ctrl.book= book;
-
+        //Part of the controller for the comment function
+        $ctrl.comment= {};
+        $ctrl.addComment= function(id, comment){
+        if(comment.author === undefined){
+        	$ctrl.emailValidate= true;
+        	return;
+        }
+        BookService.addComment(id, comment);
+        $ctrl.comment= {};
+        $state.reload();	
+        }
     }
 })();
--- ajsexercises/services/books-services.js
+++ ajsexercises/services/books-services.js
@@ -12,7 +12,15 @@
             "editorial": "El lado oscuro",
             "synopsis": "Harry Potter ingresa a la escuela Howarts de Magia y hechicería.",
             "img": "./public/img/hp1.jpg",
-            "genre": "Fantasie, Action, Sciense Fiction, Magic"
+            "genre": "Fantasie, Action, Sciense Fiction, Magic",
+            "comments":[{
+                "author" : "MiguelA.MV@outllok.com",
+                "content" : "Este libro te introduce muy bien a la saga de HP, es un inicio suave, aunque tiene sus partes profundas. Vale la pena re-leerlo."
+            },
+            {
+              "author" : "micky_moco_@hotmail.com",
+                "content" : "Primero."
+            }]
         },
         {
             "id": 2,
@@ -21,7 +29,11 @@
             "editorial": "Salamandra",
             "synopsis": "Harry vuelve al segundo año en la escuela Howarts. Después de descubrir la verdad de su pasado, Harry deberá enfrentar nuevas pruebas.",
             "img": "./public/img/hp2.jpg",
-            "genre": "Fantasie, Action, Sciense Fiction, Magic"
+            "genre": "Fantasie, Action, Sciense Fiction, Magic",
+            "comments":[{
+                "author" : "mickymoco@gmail.com",
+                "content" : "SPOILER ALERT. Me gusta como manejan el crecimiento de los personajes."
+                }]
         },
         {
             "id": 3,
@@ -30,7 +42,11 @@
             "editorial": "Oceano",
             "synopsis": "Un peligroso reo escapa de la prisión de máxima seguridad Azkaban y va a Howarts presuntamente buscando a Harry.",
             "img": "./public/img/hp3.jpg",
-            "genre": "Fantasie, Action, Sciense Fiction, Magic"
+            "genre": "Fantasie, Action, Sciense Fiction, Magic",
+            "comments":[{
+                "author" : "micky_moco_@hotmail.com",
+                "content" : "Primero."
+                }]
         },
         {
             "id": 4,
@@ -39,7 +55,11 @@
             "editorial": "Salamandra",
             "synopsis": "Los juegos de los tres magos se celebraran en Howarts, en el cuál Harry participará de una manera inesperada.",
             "img": "./public/img/hp4.jpg",
-            "genre": "Fantasie, Action, Sciense Fiction, Magic"
+            "genre": "Fantasie, Action, Sciense Fiction, Magic",
+            "comments":[{
+                "author" : "micky_moco_@hotmail.com",
+                "content" : "Primero."
+                }]
         },
         {
             "id": 5,
@@ -48,7 +68,11 @@
             "editorial": "Ventura",
             "synopsis": "2 Niños descubren el poder de unos anillos capaces de viajar entre mundos.",
             "img": "./public/img/na1.jpg",
-            "genre": "Fiction, Fantasie"
+            "genre": "Fiction, Fantasie",
+            "comments":[{
+                "author" : "micky_moco_@hotmail.com",
+                "content" : "Hasta que lo leí, no sabía que era el primer libro de la saga."
+                }]
         },
         {
             "id": 6,
@@ -57,7 +81,11 @@
             "editorial": "Oceano",
             "synopsis": "3 niños entran a un mundo mágico a traves de un Ropero y viven aventuras fantasticas.",
             "img": "./public/img/na2.jpg",
-            "genre": "Fiction, Fantasie"
+            "genre": "Fiction, Fantasie",
+            "comments":[{
+                "author" : "micky_moco_@hotmail.com",
+                "content" : "Primero."
+                }]
         },
         {
             "id": 7,
@@ -66,7 +94,11 @@
             "editorial": "Ventura",
             "synopsis": "Cuenta la historia de dos jovenes enamorados cuyas familias se oponían a su union.",
             "img": "./public/img/rj.jpg",
-            "genre": "Novel, Romance"
+            "genre": "Novel, Romance",
+            "comments":[{
+                "author" : "micky_moco_@hotmail.com",
+                "content" : "Mejor busquen a Gnomeo :v"
+                }]
         },
         {
             "id": 8,
@@ -75,7 +107,11 @@
             "editorial": "Oceano",
             "synopsis": "Colin aprovehca la segunda oportunidad que le da la vida cuando conoce a María y sus vidas cambian.",
             "img": "./public/img/ts.jpg",
-            "genre": "Novel, Romance"
+            "genre": "Novel, Romance",
+            "comments":[{
+                "author" : "micky_moco_@hotmail.com",
+                "content" : "Primero."
+                }]
         }
         /*{
             "id": 4,
@@ -84,7 +120,11 @@
             "editorial": "Salamandra",
             "synopsis": "Los juegos de los tres magos se celebraran en Howarts, en el cuál Harry participará de una manera inesperada.",
             "img": "./public/img/hp4.jpg",
-            "genre": "Romance, Comedy"
+            "genre": "Romance, Comedy",
+            "comments":[{
+                "author" : "micky_moco_@hotmail.com",
+                "content" : "Primero."
+                }]
         },*/
     ];
 
@@ -94,6 +134,7 @@
         service.getAllBooks= getAllBooks;
         service.getOneBook= getOneBook;
         service.getSimillarBooks= getSimillarBooks;
+        service.addComment= addComment;
  
         function getAllBooks(){
             return books;
@@ -103,6 +144,7 @@
             return books[id];
         }
 
+        //Custom function to get the books with the same genre, displaying a new view
         function getSimillarBooks(genre){
             var simillarBooks= books.filter(books => {
                 if (genre === books.genre) {
@@ -111,6 +153,11 @@
             });
             console.log('Aqui',simillarBooks);
             return simillarBooks;
+        }
+
+        //Custom function to add a comment in the tab-view.html
+        function addComment(id, comment){
+            books[id].comments.push(comment);
         }
 
     }
